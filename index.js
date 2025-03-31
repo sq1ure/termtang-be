898dbb81462e46f7c32fc7afb2ba113393e3d35c
supawadee.pengchan <sp.supawadeez@gmail.com>
2025-03-31 09:18:49 +0700

    mongoose connect

--- index.js
+++ index.js
@@ -34,7 +34,10 @@ app.use(cors(corsOptions));
 
 // MongoDB connection
 console.log('🚀 ~ process.env.MONGODB_URI:', process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log('MongoDB Connected'))
     .catch((err) => console.log('MongoDB connection error:', err));
 
